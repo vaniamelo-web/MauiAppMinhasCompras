a25e0694b87b9edd909bc311a933d81ea56694d9
vaniamelo-web <vanvanmelo2796@gmail.com>
2026-08-10 19:03:30 -0300

    Add no SQLiteDataBaseHelper

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SQLite;
+using MauiAppMinhasCompras.Models;
 
 namespace MauiAppMinhasCompras.Helpers
 {
-    internal class SQLiteDatabaseHelper
+    public class SQLiteDatabaseHelper
     {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path) 
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+
+        public void Insert(Produto p) { }
+
+        public void Update(Produto p) { }
+
+        public void Delete(int Id) { }
+
+        public void GetAll() { }
+
+        public void Search() { }
     }
 }
