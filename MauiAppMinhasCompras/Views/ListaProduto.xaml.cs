3a0d97525b45dcbe5bf99ae6336fe6370f7f4d00
vaniamelo-web <vanvanmelo2796@gmail.com>
2026-09-07 09:29:44 -0300

    Atualizações da agenda 5 de DS

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -18,6 +18,8 @@ public partial class ListaProduto : ContentPage
     {
         try
         {
+            lista.Clear();
+
             List<Produto> tmp = await App.Db.GetAll();
 
             tmp.ForEach(i => lista.Add(i));
